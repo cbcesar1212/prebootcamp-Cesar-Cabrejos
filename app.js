63940c546051749691a7d24e6fbb5d2da0e97d62
cbcesar1212 <chatsito1212@gmail.com>
2025-06-22 12:10:35 -0500

    Agregar estilos con archivo style.css

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.getElementById("botonSaludo").addEventListener("click", function() {
   const nombre = document.getElementById("nombre").value;
-  const mensaje = `Hola, ${nombre}`;
+  const mensaje = `Hola ${nombre}, Mucho gusto, soy César Cabrejos`;
   document.getElementById("mensaje").textContent = mensaje;
 });
